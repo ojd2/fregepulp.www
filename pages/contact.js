93b27e1f7aa864e95d6dd64f9c14026c0e0d4d60
ojd2 <ojd2@st-andrews.ac.uk>
2018-03-27 18:35:42 +0100

    Format

--- pages/contact.js
+++ pages/contact.js
@@ -8,31 +8,30 @@ export default class extends React.Component {
 	{
 	    return (
 	    	<div>
-
- 			<Header />
- 			<div class="page-contact">
- 			<div class="container">
-	 			<div grid-row="" class="scroll-transition-fade below-viewport" grid-pad="2" grid-gutter="4" grid-responsive="">
-	    			<div grid-col="12" grid-pad="2" class="">
-	    					<h1 class="details-spread" style={{textAlign: 'center', fontSize: '5rem' }}>Time to get serious</h1>
-	    			</div>
+	 			<Header />
+	 			<div class="page-contact">
+		 			<div class="container">
+			 			<div grid-row="" class="scroll-transition-fade below-viewport" grid-pad="2" grid-gutter="4" grid-responsive="">
+			    			<div grid-col="12" grid-pad="2" class="">
+			    					<h1 class="details-spread" style={{textAlign: 'center', fontSize: '5rem' }}>Time to get serious</h1>
+			    			</div>
+			    		</div>
+			    		<div grid-row="" class="scroll-transition-fade below-viewport" grid-pad="2" grid-gutter="4" grid-responsive="">
+			    			<div grid-col="4" grid-pad="2" class="">
+			    					<h3 class="details-spread" style={{textAlign: 'center', fontSize: '2rem', fontFamily: 'Times New Roman' }}>Stay with me,</h3>
+			    			</div>
+			    			<div grid-col="4" grid-pad="2" class="">
+			    					<h3 class="details-spread" style={{textAlign: 'center', fontSize: '2rem', fontFamily: 'Times New Roman' }}>Cause you are all I need...</h3>
+			    			</div>
+			    			<div grid-col="4" grid-pad="2" class="">
+			    					<h3 class="details-spread" style={{textAlign: 'center', fontSize: '2rem', fontFamily: 'Times New Roman' }}>Darling...</h3>
+			    			</div>
+			    		</div>
+		    		</div>
 	    		</div>
-	    		<div grid-row="" class="scroll-transition-fade below-viewport" grid-pad="2" grid-gutter="4" grid-responsive="">
-	    			<div grid-col="4" grid-pad="2" class="">
-	    					<h3 class="details-spread" style={{textAlign: 'center', fontSize: '2rem', fontFamily: 'Times New Roman' }}>Stay with me,</h3>
-	    			</div>
-	    			<div grid-col="4" grid-pad="2" class="">
-	    					<h3 class="details-spread" style={{textAlign: 'center', fontSize: '2rem', fontFamily: 'Times New Roman' }}>Cause you are all I need...</h3>
-	    			</div>
-	    			<div grid-col="4" grid-pad="2" class="">
-	    					<h3 class="details-spread" style={{textAlign: 'center', fontSize: '2rem', fontFamily: 'Times New Roman' }}>Darling...</h3>
-	    			</div>
-	    		</div>
-    		</div>
-    		</div>
-    		<Social />
-    		<Footer />
-    		<div class="page-background-contact"></div>
+	    		<Social />
+	    		<Footer />
+	    		<div class="page-background-contact"></div>
   			</div>
 	    )
 	}
